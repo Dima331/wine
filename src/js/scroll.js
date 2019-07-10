730fed305d2a1c76a843137747c816c34580ec8e
Дмитрий Барков <C:\Users\Barkov_D\AppData\Roaming\The Bat!>
2019-07-10 16:41:35 +0300

    ref code

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,20 +1,20 @@
-(function() {
-    $(".order").click(function() {
+(function () {
+    $(".order").click(function () {
         $("html, body").animate({
             scrollTop: $(".wines").offset().top + "px"
         }, {
-            duration: 500,
-            easing: "swing"
-        });
+                duration: 500,
+                easing: "swing"
+            });
         return false;
     });
-    $(".arrow-scroll").click(function() {
+    $(".arrow-scroll").click(function () {
         $("html, body").animate({
             scrollTop: $(".full-taste").offset().top + "px"
         }, {
-            duration: 500,
-            easing: "swing"
-        });
+                duration: 500,
+                easing: "swing"
+            });
         return false;
     });
 })()
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     $('.wines-list').slick({
         appendArrows: $('.wines-navigation'),
         nextArrow: $('.wines-navigation__next'),
@@ -6,32 +6,30 @@
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [{
-                breakpoint: 1050,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-
-            
-            {
-                breakpoint: 851,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    variableWidth: true
-                }
-            },
-            {
-                breakpoint: 660,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    variableWidth: true
-                }
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
             }
+        },
+        {
+            breakpoint: 851,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+        },
+        {
+            breakpoint: 660,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                variableWidth: true
+            }
+        }
         ]
     });
 })()
